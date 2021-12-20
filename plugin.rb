6cd2a5082360eeda3272302049c5ce30ccc6c7ec
Thomas Kalka <thomas.kalka@gmail.com>
2021-12-20 08:39:01 +0100

    better parameters

--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,6 @@
 # name: Discourse geogebra Onebox
 # about: Adds support for embedding geogebra calculators within Discourse.
-# version: 0.1
+# version: 0.0.2
 # authors: Thomas Kalka
 # url: https://github.com/thoka/discourse-geogebra-onebox
 
@@ -17,7 +17,10 @@ class Onebox::Engine::GeogebraOnebox
 	end
 	
 	def to_html
-        "<iframe name='embed_readwrite' src='https://www.geogebra.org/calculator/#{id}?embed' width='800' height='600'"\
-        " allowfullscreen style='border: 1px solid #e4e4e4;border-radius: 4px; transform: scale(0.5);' frameborder='0'></iframe>"
+        # For iframe parameters see https://wiki.geogebra.org/en/Reference:Material_Embedding_(Iframe)
+        "<div style='left: 0; width: 100%; height: 0; position: relative; padding-bottom: 63.0354%;'>"\
+        "<iframe src='https://www.geogebra.org/material/iframe/id/#{id}/sfsb/true/width/1200/height/760/szb/true/rc/true/sdz/true/sri/true/ai/true' "\
+        "style='top: 0; left: 0; width: 100%; height: 100%; position: absolute; border: 0;' allowfullscreen>"\
+        "</iframe></div>"
 	end
 end
